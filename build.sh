cc1fe590365c2f66f740b93b4dc1fbb183433368
Subash Pathak <Subash@users.noreply.github.com>
2020-06-09 18:49:24 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-HASH=`git rev-parse --short HEAD`
-docker build -t "subash/bs.subashpathak.com:$HASH" .
-docker push "subash/bs.subashpathak.com:$HASH"
+hash=`git rev-parse --short HEAD`
+docker build -t "subash/bs.subashpathak.com:$hash" .
+docker push "subash/bs.subashpathak.com:$hash"
